965c34cc7de55a4af3087762e07ae3b30f2e1f5b
martees <60579713+martees@users.noreply.github.com>
2020-05-06 07:04:14 +0200

    effect of time + effect of ratio

--- progdyn5_Fearbased_Main.py
+++ progdyn5_Fearbased_Main.py
@@ -14,38 +14,52 @@ exec(open("param.txt").read())
 #Function that returns the fitness-maximizing a* for E=S and E=R
 #It is numerically computable by finding the maximum of the fitness function F (reached at the derivative's root)
 def NumericOptimal():
+    exec(open("param.txt").read(),globals())
+
     SAFE =  (b1*gamma_S*exp(-b1*(a - m1)))/((exp(-b1*(a - m1)) + 1)**2 (exp(-b2*(a - m2)) + 1)) + (b2*exp(-b2*(a - m2))*(gamma_S/(exp(-b1*(a - m1)) + 1) - gamma_S + 1))/(exp(-b2*(a - m2)) + 1)**2
     RISKY = (b1*gamma_R*exp(-b1*(a - m1)))/((exp(-b1*(a - m1)) + 1)**2 (exp(-b2*(a - m2)) + 1)) + (b2*exp(-b2*(a - m2))*(gamma_R/(exp(-b1*(a - m1)) + 1) - gamma_R + 1))/(exp(-b2*(a - m2)) + 1)**2
-    return([SAFE,RISKY])
 
+    return([SAFE,RISKY])
 
 ## Effect of average residence time in each environment on optimal perfectly informed strategy
 def EffectOfTime():
-    T=1000 #maximum mean residence time tested
-    stability = np.zeros((T,2))
-    for t in range(1,T):
-        Set("param.txt","transition_S",str((t-1)/T))
-        Set("param.txt","transition_R",str((t-1)/T))
+    step=0.001 #step at which the transition rate varies
+    n = int(1/step) #number of transition rates to test for
+    stability = np.zeros((n,2))
+    axis=np.zeros((n,2))
+    for t in range(1,n):
+        Set("param.txt","transition_S",str(t*step))
+        Set("param.txt","transition_R",str(t*step))
         stability[t][0],stability[t][1] = PerfectInfo()
-    plt.plot(stability[1:])
+        axis[t]=[t*step,t*step]
+    plt.plot(axis[1:],stability[1:])
+    plt.xlabel('transition probability')
+    plt.ylabel('optimal a with perfect info')
     plt.xscale('log')
+    plt.gca().invert_xaxis()
+    #plt.gca().set_ylim([0,1])
     plt.legend(['Good','Bad'])
     plt.show()
-
+#EffectOfTime()
 
 ## Effect of ratio of riskiness between the two envts on optimal perfectly informed strategy
 def EffectOfRatio():
     R=10 #maximum ratio tested
     step=0.1 #ratio granularity
     stability = np.zeros((int(R/step),2))
+    axis=np.zeros((int(R/step),2))
     Set("param.txt","gamma_S",str(0.1000))
     for r in np.arange(1,R/step).astype(int):
-        Set("param.txt","gamma_R",str(0.1*int(r/R)))
+        Set("param.txt","gamma_R",str(step*int(r/R)))
         stability[r][0],stability[r][1] = PerfectInfo()
-    plt.plot(stability[1:])
+        axis[r]=[step*int(r/R),step*int(r/R)]
+    plt.plot(axis[1:],stability[1:])
+    plt.xlabel('ratio R/S')
+    plt.ylabel('optimal a with perfect info')
+    #plt.gca().set_ylim([0,1])
     plt.legend(['Good','Bad'])
     plt.show()
-
+EffectOfRatio()
 
 
 ## Simulation
