7e2e2bbd146c08999606106f25122edc2dcb0ba0
martees <60579713+martees@users.noreply.github.com>
2020-05-25 15:59:33 +0200

    typo

--- DP3_Fearbased_PerfectInfo.py
+++ DP3_Fearbased_PerfectInfo.py
@@ -8,7 +8,6 @@ from scipy.special import lambertw
 
 
 ##Total fitness obtained through a given time step from performing a in environment E
-#This function is used in Main to compare the performances of the different heuristics.
 def F(a,E):
     if E=='S':
         gamma = gamma_S
@@ -18,7 +17,7 @@ def F(a,E):
 
 
 ##Long-term reproductive value of an animal performing intensity a in environment E
-def H1(a,V,E):
+def H(a,V,E):
     #adjust parameters depending on the value of E
     if E=='S':
         h = ((1-transition_S)*F(a,'S')*V[0])+(transition_S*F(a,'R')*V[1])
@@ -28,11 +27,11 @@ def H1(a,V,E):
 
 
 ## Dynamic programming operator (finds the a that maximizes V for each E, and fills up V and A with the corresponding max)
-def T1(V,E):
+def T(V,E):
     maxi=0 #maximum H
     maxa=0 #optimal a
     for a in alist: #for many a's
-        t = H1(a,V,E) #calculate the reproductive value if foraging at a in E
+        t = H(a,V,E) #calculate the reproductive value if foraging at a in E
         if t>maxi: #if a better t is found, update maxima
             maxi=t
             maxa=a
@@ -42,7 +41,7 @@ def T1(V,E):
 ## Find optimal strategy for Perfect Information, as the limit of a sequence of functions
 def PerfectInfo():
     #V = relative probability of survival for any long period under a optimal strategy
-    #newVe are going to build a function sequence in order to obtain the optimal V(E) [ 0 = SAFE, 1 = RISKY ]
+    #We build a function sequence in order to obtain the optimal V(E) [ 0 = SAFE, 1 = RISKY ]
     exec(open("param.txt").read(),globals()) #executing parameter file
     V = [1,1]
     newV = [1,1]
@@ -53,8 +52,8 @@ def PerfectInfo():
         V=deepcopy(newV) #previous newV is stored in V
         newV = np.zeros(2) #current newV is initialized
         #main calculation
-        newV[0], a_max[0] = T1(V,'S')
-        newV[1], a_max[1] = T1(V,'R')
+        newV[0], a_max[0] = T(V,'S')
+        newV[1], a_max[1] = T(V,'R')
         #Normalization
         maxi = np.amax(newV)
         if maxi != 0:
