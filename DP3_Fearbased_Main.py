5204a76ad5730e337830c1c880634513ab2dc9a9
martees <60579713+martees@users.noreply.github.com>
2020-05-31 07:14:37 +0200

    added analysis functions

--- DP3_Fearbased_Main.py
+++ DP3_Fearbased_Main.py
@@ -12,7 +12,7 @@ import pandas as pd
 from matplotlib.patches import Patch
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-
+import matplotlib.colors as colors
 
 from DP3_Parameters import *
 Create("param.txt")
@@ -166,6 +166,8 @@ def c_d_Matrix():
     # this is the value to use for the color
     V = np.amax(stability,axis = -1)
 
+    plt.xlabel('gamma_R')
+
     # create the figure, add a 3d axis, set the viewing angle
     fig = plt.figure()
     ax = plt.axes(projection='3d')
@@ -181,8 +183,23 @@ def c_d_Matrix():
 #stab = c_d_Matrix()
 
 ## Optimal strategies comparison
-
 #a = np.genfromtxt('OptPINa10000.csv', delimiter=',')
+#np.savetxt("OptA-Gauge-d_c.csv", opt, delimiter=",")
+def StratCompPlot():
+    OptPI = pi.PerfectInfo()
+    OptBay = bay.Bayesian()
+    OptGauge = gau.Dummy()
+
+    plt.axhline(y=OptPI[0], label = "P_S", color = 'gold')
+    plt.axhline(y=OptPI[1], label = "P_R", color = 'goldenrod')
+    plt.plot(np.arange(0,L+L/N,L/N), OptBay, label = "Bayesian", color = 'royalblue')
+    plt.plot(OptGauge, label = "Gauge", color = 'blue')
+
+    plt.xlabel('Gauge percentage / Bayesian Estimate')
+    plt.ylabel('Optimal a')
+    plt.legend()
+
+    plt.show()
 
 ##Immediate fitness
 def F(a,pred):
@@ -195,6 +212,7 @@ def F(a,pred):
 def EnvPredGen(T):
     'Function that generates a random sequence of environmental states + pred events based on current parameters'
     environment = np.zeros(T)
+    pred_encounter = np.zeros(T)
     rand = 1000
     threshold1_S = int(rand*transition_S) #threshold below which the environment switches => risky
     threshold1_R = int(rand*transition_R) #threshold below which the environment switches => safe
@@ -234,12 +252,13 @@ def EnvPredGen(T):
 
 
 ## Simulation for perfectly informed agents
-def Sim_PerfectInfo(T,IsEnv=False,environment=0,pred_encounter=0):
+def Sim_PerfectInfo(T,IsEnv=False,environment=0,pred_encounter=0,IsOpt=False,OptA=0):
     'Returns the accumulated fitness of an agent following a perfectly informed strategy during a period of length T generated using current paremeters (stochastically).'
     exec(open("param.txt").read(),globals()) #executing parameter file
 
     #First, we run the method until an optimal strategy is found by convergence
-    OptA = pi.PerfectInfo()
+    if IsOpt == False:
+        OptA = pi.PerfectInfo()
     #Storage tables
     resulting_a = np.zeros(T) #to store the a chosen by the animal at each time step
     local_fitness = np.zeros(T) #to store the resulting fitness of the animal's behavior
@@ -311,12 +330,13 @@ def Plot_Sim_PerfectInfo(T):
     plt.show()
 
 ## Simulation for bayesian agents
-def Sim_Bayesian(T,IsEnv=False,environment=0,pred_encounter=0):
+def Sim_Bayesian(T,IsEnv=False,environment=0,pred_encounter=0, IsOpt=False, OptA=0):
     'Returns the accumulated fitness of an agent following a bayesian strategy during a period of length T generated using current paremeters (stochastically).'
     exec(open("param.txt").read(),globals()) #executing parameter file
 
     #First, we run the method until an optimal strategy is found by convergence
-    OptA = bay.Bayesian()
+    if IsOpt == False:
+        OptA = bay.Bayesian()
     #Storage tables
     if IsEnv==False:
         environment = np.zeros(T) #to store the sequence of environments
@@ -604,49 +624,240 @@ def c_d_Perf_Matrix():
     safe_pred_enc[::t2_r]=1
     pred_encounter = np.concatenate((safe_pred_enc,risky_pred_enc,safe_pred_enc,risky_pred_enc))
 
-    step=20 #step with which the c and d vary until L
-    n = int(L/step) #total number of values to test for in each variable
-    Opt = np.zeros((n,n,L+1))
-    fit = np.zeros((n,n))
-    axis=np.zeros((n,2))
-    for inc in range(1,n):
-        Set("param.txt","c",str(inc*step))
-        fincor dec in range():
-            Set("param.txt","d",str(dec*step))
-            Opt[inc,dec] = gau.Dummy()
-            print('c=',inc*step,'d=',dec*step, 'a=', Opt[inc,dec,int(L/2)])
-            #Simulation results
-            fit[inc,dec] = Sim_Gauge(len(environment),IsEnv=True,environment=environment, pred_encounter=pred_encounter, IsOpt=True, OptA=Opt[inc,dec])[2]
+    step=1 #step with which the c and d vary until L
+    lim_d_1 = 0
+    lim_d_2 = 10
+    lim_c_1 = 70
+    lim_c_2 = 90
+    n_c = int((lim_c_2-lim_c_1)/step) #total number of values to test for in each variable
+    n_d = int((lim_d_2-lim_d_1)/step) #total number of values to test for in each variable
+    Opt = np.zeros((n_c,n_d,L+1))
+    fit = np.zeros((n_c,n_d))
 
-    x = y = np.arange(0, L, step)
+    for inc in range(lim_c_1,lim_c_2,step):
+        i = int((inc-lim_c_1)/step)
+        Set("param.txt","c",str(inc*step))
+        for dec in range(lim_d_1,lim_d_2,step):
+            j = int((dec-lim_d_1)/step)
+            Set("param.txt","d",str(dec*step))
+            Opt[i,j] = gau.Dummy()
+            print('c=',inc*step,'d=',dec*step, 'a=', Opt[i,j,int(L/2)])
+            #Simulation results
+            fit[i,j] = Sim_Gauge(len(environment),IsEnv=True,environment=environment, pred_encounter=pred_encounter, IsOpt=True, OptA=Opt[i,j])[2]
+
+    #csv storage
+    #for i in range(len(Opt)):
+    #    np.savetxt("OptA-Gauge-d_c-"+i+".csv", Opt[i], delimiter=",")
+
+    return(Opt,fit)
+
+def Opt_csv_to_arr(n):
+    Opt=np.zeros((n,n,L+1))
+    for i in range(n):
+        a = np.genfromtxt("OptA-Gauge-d_c-"+str(i)+".csv", delimiter=",")
+        Opt[i]=a
+    return(Opt)
+
+def Sim_from_Opt(Opt):
+    #average length of stable environmental period
+    t1_s = int(1/transition_S)
+    t1_r = int(1/transition_R)
+    #average time between two predator encounters in each environment
+    t2_s = int(1/gamma_S)
+    t2_r = int(1/gamma_R)
+
+    #Environment is composed of average-lengthed stable periods
+    environment = np.concatenate((np.zeros(t1_s),np.ones(t1_r),np.zeros(t1_s),np.ones(t1_r)))
+    #Predator encounters are averagely spaced throughout all periods
+    safe_pred_enc = np.zeros(t1_s)
+    safe_pred_enc[::t2_s]=1
+    risky_pred_enc = np.zeros(t1_r)
+    safe_pred_enc[::t2_r]=1
+    pred_encounter = np.concatenate((safe_pred_enc,risky_pred_enc,safe_pred_enc,risky_pred_enc))
+
+    step=1 #step with which the c and d vary until L
+    lim_c_1 = 70
+    lim_c_2 = 90
+    lim_d_1 = 0
+    lim_d_2 = 10
+    n_c = int((lim_c_2-lim_c_1)/step) #total number of values to test for in each variable
+    n_d = int((lim_d_2-lim_d_1)/step) #total number of values to test for in each variable
+    fit = np.ones((n_c,n_d))
+
+    for inc in range(lim_c_1,lim_c_2,step):
+        i = int((inc-lim_c_1)/step)
+        Set("param.txt","c",str(inc*step))
+        for dec in range(lim_d_1,lim_d_2,step):
+            j = int((dec-lim_d_1)/step)
+            Set("param.txt","d",str(dec*step))
+            #Simulation results
+            fit[i,j] = Sim_Gauge(len(environment),IsEnv=True,environment=environment, pred_encounter=pred_encounter, IsOpt=True, OptA=Opt[i,j])[2]
+
+    return(fit)
+
+def c_d_Perf_Plot(fit):
+    step=1 #step with which the c and d vary until L
+    lim_d_1 = 0
+    lim_d_2 = 10
+    lim_c_1 = 70
+    lim_c_2 = 90
+    n_d = int((lim_d_2-lim_d_1)/step) #total number of values to test for in each variable
+    n_c = int((lim_c_2-lim_c_1)/step) #total number of values to test for in each variable
+
+    #Opt,fit = c_d_Perf_Matrix()
+
+    x = np.arange(0, n_d, step)+lim_d_1
+    y = np.arange(0, n_c, step)+lim_c_1
     # here are the x,y and respective z values
     X, Y = np.meshgrid(x, y)
-    Z = np.array(fit)
+    #Z = fit/np.amax(fit)
+    Z = ((fit/np.amax(fit))-0.9999)*10000
 
     # create the figure, add a 3d axis, set the viewing angle
     fig = plt.figure()
     ax = plt.axes(projection='3d')
     ax.view_init(45,60)
+    ax.set_xlabel('d')
+    ax.set_ylabel('c')
+    ax.set_zlabel('Relative fitness (normalized)')
+
+    ax.set_xticks(np.arange(lim_d_1,lim_d_2,step))
+    ax.set_yticks(np.arange(lim_c_1,lim_c_2,step))
+    #ax.set_zticks(np.arange(0.00006,0.00009,0.00001))
+
+    plt.setp(ax.get_yticklabels()[::2], visible=False)
+
+    norm = colors.Normalize()
 
     # here we create the surface plot, but pass V through a colormap
     # to create a different color for each patch
-    ax.plot_surface(X, Y, Z, facecolors=cm.Oranges(Z))
+    ax.plot_surface(X, Y, Z, facecolors=cm.viridis(norm(Z)))
+    #ax.scatter(0,7*step,Z[7,0])
+    #ax.scatter(0,8*step,Z[8,0])
 
     plt.show()
-    return(stability)
 
-stab = c_d_Perf_Matrix()
 
+#opt = c_d_Perf_Matrix()
+
+#np.savetxt("OptA-Gauge-d_c.csv", opt, delimiter=",")
 
 ## Comparison
 
 #Reminder
-#Sim_PerfectInfo: returns (environment,resulting_a,local_fitness,global_fitness)
-#Sim_Bayesian: returns (environment,resulting_a,local_fitness,global_fitness, resulting_p, pred_encounter)
-#Sim_Gauge: returns (environment,resulting_a,local_fitness,global_fitness, resulting_g, pred_encounter)
+#Sim_PerfectInfo: returns (resulting_a,local_fitness,global_fitness)
+#Sim_Bayesian: returns (resulting_a,local_fitness,global_fitness, resulting_p)
+#Sim_Gauge: returns (resulting_a,local_fitness,global_fitness, resulting_g)
 
-#def GlobalFitComp():
+def GlobalSimulation(T):
+    #envt and pred events generation
+    environment, pred_encounter = EnvPredGen(T)
+    #Time table
+    t = np.around(np.arange(0,T,1),1)
 
+    PI_resulting_a,PI_local_fitness,PI_global_fitness = Sim_PerfectInfo(T,IsEnv=True, environment=environment, pred_encounter=pred_encounter)
+    B_resulting_a,B_local_fitness,B_global_fitness,resulting_p = Sim_Bayesian(T,IsEnv=True, environment=environment, pred_encounter=pred_encounter)
+    G_resulting_a,G_local_fitness,G_global_fitness, resulting_g = Sim_Gauge(T,IsEnv=True, environment=environment, pred_encounter=pred_encounter)
+
+    #Plotting
+
+    #Plot environment
+    env = plt.subplot(511)
+    #Make colors out of environment list
+    colors = list(map(lambda x: "yellow" if x else "green", environment)) #green if safe, yellow is risky
+    plt.bar(t,np.ones(T),width=1.0, color=colors) #barplot without inter-bar spaces
+    env.set_ylabel('Environment')
+    #Legend (built manually)
+    legend_elements = [Patch(facecolor='green',edgecolor='white', label='Safe'), Patch(facecolor='yellow', edgecolor='black',label='Risky')]
+    env.legend(handles=legend_elements, loc='upper right')
+
+    #Plotting chosen antipredator behavior a for the three strategies
+    a = plt.subplot(512,sharex=env)
+    plt.plot(PI_resulting_a, color= 'gold', label = 'P')
+    plt.plot(B_resulting_a, color= 'royalblue', label = 'B')
+    plt.plot(G_resulting_a, color= 'goldenrod', label = 'G')
+
+    plt.eventplot(t*pred_encounter,lineoffsets = 0.5, linelengths = 1, color = 'gray', linewidth=0.5)
+    plt.setp(a.get_xticklabels(), visible=False)
+    plt.ylim(0.524, 0.56)
+    a.set_ylabel('Resulting a')
+
+    plt.legend()
+
+    #Plotting the resulting gauge level g
+    g = plt.subplot(513,sharex=env)
+    plt.plot(resulting_g/L)
+    plt.eventplot(t*pred_encounter,lineoffsets = 0.5, linelengths = 1, color = 'gray', linewidth=0.5)
+    plt.setp(g.get_xticklabels(), visible=False)
+    plt.ylim(0, 1)
+    g.set_ylabel('Resulting gauge level g')
+    g.fill_between(t, resulting_g/L, color='yellow')
+    g.fill_between(t, resulting_g/L, np.ones(T), color='green')
+
+    #Plotting the resulting estimate p
+    p = plt.subplot(514,sharex=env)
+    plt.plot(resulting_p/N)
+    plt.eventplot(t*pred_encounter,lineoffsets = 0.5, linelengths = 1, color = 'gray', linewidth=0.5)
+    plt.setp(p.get_xticklabels(), visible=False)
+    plt.ylim(0, 1)
+    p.set_ylabel('Resulting estimate p')
+    p.fill_between(t, resulting_p/N, color='green')
+    p.fill_between(t, resulting_p/N, np.ones(T), color='yellow')
+
+    #Plotting the local fitness
+    localf= plt.subplot(515,sharex=env)
+    plt.plot(PI_local_fitness, color= 'gold', label = 'P')
+    plt.plot(B_local_fitness, color= 'royalblue', label = 'B')
+    plt.plot(G_local_fitness, color= 'goldenrod', label = 'G')
+    plt.eventplot(t*pred_encounter,lineoffsets = 0.5, linelengths = 1, color = 'gray', linewidth=0.5)
+    plt.setp(localf.get_xticklabels(), visible=False)
+    plt.ylim(0.98, 1)
+    localf.set_ylabel('Local Fitness')
+
+    plt.show()
+
+
+def GlobalFitComp(T,n):
+    global_fit = np.zeros((n,3))
+
+    #computing optimal strats once and for all
+    OptPI = pi.PerfectInfo()
+    OptB = bay.Bayesian()
+    OptG = gau.Dummy()
+
+    for test in range(n):
+        #envt and pred events generation
+        environment, pred_encounter = EnvPredGen(T)
+        #Time table
+        t = np.around(np.arange(0,T,1),1)
+        PI_resulting_a,PI_local_fitness,PI_global_fitness = Sim_PerfectInfo(T,IsEnv=True, environment=environment, pred_encounter=pred_encounter,IsOpt = True, OptA=OptPI)
+        B_resulting_a,B_local_fitness,B_global_fitness,resulting_p = Sim_Bayesian(T,IsEnv=True, environment=environment, pred_encounter=pred_encounter,IsOpt= True, OptA=OptB)
+        G_resulting_a,G_local_fitness,G_global_fitness, resulting_g = Sim_Gauge(T,IsEnv=True, environment=environment, pred_encounter=pred_encounter,IsOpt = True, OptA = OptG)
+
+        global_fit[test] = PI_global_fitness, B_global_fitness, G_global_fitness
+
+    meanPI = np.mean(global_fit[:,0])
+    meanB = np.mean(global_fit[:,1])
+    meanG = np.mean(global_fit[:,2])
+
+    stdevPI = np.std(global_fit[:,0])
+    stdevB = np.std(global_fit[:,1])
+    stdevG = np.std(global_fit[:,2])
+
+    labels = ['Perfect Info', 'Bayesian', 'Gauge']
+    x = np.arange(3)
+    heights = np.array((meanPI,meanB,meanG))
+    heights = heights/T
+    heights = heights/np.amax(heights)
+    errors = np.array((stdevPI,stdevB,stdevG))
+
+    # Build the plot
+    fig, ax = plt.subplots()
+    ax.bar(x, heights, yerr=errors, align='center', alpha=0.5, ecolor='black', capsize=10, color = ['gold','royalblue','goldenrod'])
+    ax.set_ylabel('Average Fitness per time unit')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.yaxis.grid(True)
 
 
 
